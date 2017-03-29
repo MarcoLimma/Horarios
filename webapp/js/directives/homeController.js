486bc1f3eac23e2c77dd553612d91f34ae96849b
Marco <marco.lima.id@gmail.com>
2017-03-29 15:23:48 -0300

    Outros diretótios

--- webapp/js/directives/homeController.js
+++
@@ -1,5 +0,0 @@
-angular.module('app').controller('HomeCtrl', ['$scope', '$sce', '$window', 'AuthenticationService', 'toastr',
-    function HomeCtrl($scope, $sce, $window, AuthenticationService, toastr) {
-        //teste
-    }
-]);
